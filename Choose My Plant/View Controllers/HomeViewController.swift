28840a5fab1ea3898758adfd60cc4263509117eb
Matthew Lock <matthew_lock20@hotmail.com>
2020-08-24 15:07:17 +0100

    Randomizer with URL working

--- Choose My Plant/View Controllers/HomeViewController.swift
+++ Choose My Plant/View Controllers/HomeViewController.swift
@@ -31,6 +31,7 @@ class HomeViewController: UIViewController {
     
     
     var plantArray = [String]()
+    var imageArray = [String]()
     
      func getData(from url: String) {
         let task = URLSession.shared.dataTask(with: URL(string: url)!, completionHandler: { data, response, error in
@@ -42,8 +43,9 @@ class HomeViewController: UIViewController {
             do {
 
                 let result = try JSONDecoder().decode(Response.self, from: data)
-                
-                for plant in result.data { self.plantArray.append(plant.common_name) }
+
+                for plant in result.data { self.plantArray.append( plant.common_name ); self.imageArray.append( plant.image_url)
+                }
             }
                 
             catch {
@@ -58,28 +60,33 @@ class HomeViewController: UIViewController {
             struct Response: Codable {
                 struct Plant: Codable {
                     var common_name:String
+                    var image_url:String
                 }
                 var data:[Plant]
             }
+
             })
         
             task.resume()
 
     }
     var select = String()
+    var array = [String]()
     
     @IBAction func plantButtonTapped(_ sender: Any) {
-            changingTextView.text = plantChooser()
+        changingTextView.text = plantChooser()
         }
-            func plantChooser() -> String {
-                 
-                select = plantArray.randomElement()!
-                print("This is a string")
+    func plantChooser() -> String {
+        
+        for (name, image) in zip(plantArray, imageArray) {
+            array.append(contentsOf: ["\(name), \(image)"])
+            select = array.randomElement()!
+
+        }
                 return(select)
-                //return( plantArray.randomElement()!)
         }
     
-    var wishList = [String]()
+    var wishList = String()
     
     @IBAction func saveToWishlistButtonTapped(_ sender: Any) {
         self.wishList.append(select)
