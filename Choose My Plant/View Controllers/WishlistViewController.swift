287ba949c43a6c2ff334ea59ad0d120c1ac64fe0
Katie Jones <katieljones29@gmail.com>
2020-08-26 12:11:07 +0100

    capitalized plants

--- Choose My Plant/View Controllers/WishlistViewController.swift
+++ Choose My Plant/View Controllers/WishlistViewController.swift
@@ -60,7 +60,7 @@ class WishlistViewController: UIViewController, UITextViewDelegate {
     
     func printPlant() -> String {
                let string = names.joined(separator: "\n\n")
-        return("\(string)").uppercaseString
+        return("\(string.capitalized)")
            }
     
 
